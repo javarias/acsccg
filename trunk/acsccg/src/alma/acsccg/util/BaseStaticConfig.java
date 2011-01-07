d79484c364ef0bd3a831ce85aa597d155978f56a
alexis.tejeda <alexis.tejeda@5b5b498a-2085-49f7-cba5-79b24f1055b6>
2011-01-07 02:15:10 +0000

    Folder structure reorganized - several files for the build process was updated, now the jar is generated into bin folder, and libraries was moved to lib in the root folder of the project not in the src, the paths src/cl/alma/acs/ccg.. etc... this was removed and now the ..src/alma/acsccg/.., Eclipse libraries paths to develop must be updated when the projects is opened with the eclipse, but this is only for developers.

---
+++ trunk/acsccg/src/alma/acsccg/util/BaseStaticConfig.java
@@ -0,0 +1,49 @@
+/**
+ * ACS Component Code Generator - http://code.google.com/p/acsccg/
+ * Copyright (C) 2010  Alexis Tejeda, alexis.tejeda@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ *  who       			when      			what
+ * --------				--------					----------------------------------------------
+ * atejeda 			2010-00-00  		Created
+ * 
+ */
+package acsccg.util;
+
+import java.io.File;
+
+/**
+ * Static config variables for the generator
+ * @author atejeda
+ */
+public final class BaseStaticConfig 
+{
+	
+	//private var for internal usage
+	private final static String BASE_MWE_DIR = "cl/alma/acs/ccg/mwe";
+	
+	public final static String MWE_JAVA = BASE_MWE_DIR + "/JavaWorkflow.mwe";
+	
+	public final static String MWE_CPP = BASE_MWE_DIR + "/CppWorkflow.mwe";
+	
+	public final static String MWE_PYTHON = BASE_MWE_DIR + "/PythonWorkflow.mwe";
+	
+	public final static String MWE_MODELINFORMATION = BASE_MWE_DIR + "/ModelInformation.mwe";
+	
+	public final static String LO4J_PROPERTIES = "log4j.properties";
+	
+	public final static String TEMP_OS_DIR = System.getProperty("java.io.tmpdir");
+	
+	public final static String ACSCCG_LOGGER = "cl.alma.acs.ccg";
+	
+	public final static int WORD_WRAP_MAX = 10;
+}
---
+++ trunk/acsccg/src/alma/acsccg/vo/VOGenerator.java
@@ -0,0 +1,192 @@
+/**
+ * ACS Component Code Generator - http://code.google.com/p/acsccg/
+ * Copyright (C) 2010  Alexis Tejeda, alexis.tejeda@gmail.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ *  who       			when      			what
+ * --------				--------					----------------------------------------------
+ * atejeda 			2010-00-00  		Created
+ * 
+ */
+package acsccg.vo;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Value Object for the model and profile
+ * @author atejeda
+ */
+public class VOGenerator 
+{
+
+	private String model;
+	private String profile;
+	private String output;
+	private String acspackage;
+	
+	private final static  String protocol = "file:///";
+
+	/**
+	 * Constructor
+	 * @param model
+	 * @param profile
+	 * @param output
+	 */
+	public VOGenerator(String model, String profile, String output, String acspacakge) 
+	{
+		this.model = model;
+		this.profile = profile;
+		this.output = output;
+		this.acspackage = acspacakge;
+	}
+	
+	/**
+	 * Constructor
+	 * @param model
+	 * @param profile
+	 * @param output
+	 */
+	public VOGenerator(String model, String profile, String output) 
+	{
+		this.model = model;
+		this.profile = profile;
+		this.output = output;
+	}
+	
+	/**
+	 * Constructor
+	 * @param model
+	 * @param profile
+	 */
+	public VOGenerator(String model, String profile) 
+	{
+		this.model = model;
+		this.profile = profile;
+	}
+
+	/**
+	 * Return the model path
+	 * @return String
+	 */
+	public String getModel() 
+	{
+		return model;
+	}
+
+	/**
+	 * Set the model path
+	 * @param model
+	 */
+	public void setModel(String model) 
+	{
+		this.model = model;
+	}
+
+	/**
+	 * Get the profile
+	 * @return the profile
+	 */
+	public String getProfile() 
+	{
+		return profile;
+	}
+
+	/**
+	 * Set the profile path
+	 * @param profile
+	 */
+	public void setProfile(String profile) 
+	{
+		this.profile = profile;
+	}
+
+	/**
+	 * Get output path
+	 * @return String
+	 */
+	public String getOutput() 
+	{
+		return output;
+	}
+
+	/**
+	 * Set the output path
+	 * @param output
+	 */
+	public void setOutput(String output) 
+	{
+		this.output = output;
+	}
+	
+	/**
+	 * Set the package name
+	 * @return
+	 */
+	public String getAcspackage() 
+	{
+		return acspackage;
+	}
+
+	/**
+	 * Get the package name
+	 * @param acspackage
+	 */
+	public void setAcspackage(String acspackage) 
+	{
+		this.acspackage = acspackage;
+	}
+	
+	/**
+	 * Get the model formated for internal EMF environment
+	 * @return String
+	 */
+	public String getWellFormedModel() 
+	{
+		File modelFileObj = new File(this.model);
+	    try {
+			System.out.println(modelFileObj.getCanonicalPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			return protocol+modelFileObj.getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+	
+	/**
+	 * Get the model formated for internal EMF environment
+	 * @return String
+	 */
+	public String getWellFormedProfile()
+	{
+		File modelFileObj = new File(this.profile);
+		try {
+			System.out.println(modelFileObj.getCanonicalPath());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			return protocol+modelFileObj.getCanonicalPath();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "";
+	}
+}
